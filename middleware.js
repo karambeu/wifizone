d2b7e459645ccaac8f7a1c0c73d8fa8e89038387
karambeu <cyroassi@gmail.com>
2024-08-14 08:52:08 +0000

    repair middleware

--- middleware.js
+++ middleware.js
@@ -1,31 +1,10 @@
-import { NextResponse } from 'next/server';
-import { getToken } from 'next-auth/jwt';
+export { default } from "next-auth/middleware"
 
-export async function middleware(req) {
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-  const { pathname } = req.nextUrl;
+export const config = { matcher: [
+    "/dashboard",
+    "/register",
+    "/state",
+    "/pay"
+]}
 
-  // Si l'utilisateur est authentifié, permettre l'accès
-  if (token) {
-    return NextResponse.next();
-  }
 
-  // Redirection vers la page de connexion si non authentifié
-  if (pathname.startsWith('/dashboard') || pathname.startsWith('/register') || pathname.startsWith('/state') || pathname.startsWith('/pay')) {
-    const url = new URL('/auth/signin', req.url);
-    url.searchParams.set('callbackUrl', req.nextUrl.pathname);
-    return NextResponse.redirect(url);
-  }
-
-  // Si aucune règle ne correspond, continuer
-  return NextResponse.next();
-}
-
-export const config = {
-  matcher: [
-    '/dashboard',
-    '/register',
-    '/state',
-    '/pay',
-  ],
-};
